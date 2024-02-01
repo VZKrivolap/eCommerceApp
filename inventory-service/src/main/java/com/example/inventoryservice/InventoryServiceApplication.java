9a7ec248f678ec3fe1a6f2ac09ca629b0fb688a3
VZKrivolap <vz984762@gmail.com>
2024-02-02 02:22:16 +0300

    Added Service Discovery

--- inventory-service/src/main/java/com/example/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/example/inventoryservice/InventoryServiceApplication.java
@@ -1,10 +1,8 @@
 package com.example.inventoryservice;
 
-import com.example.inventoryservice.repository.InventoryRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 public class InventoryServiceApplication {
